48cb8ca481e4e3cb24ed220864eaf0f2da947001
XiaoLong Chen <xiaolong.chen01@sap.com>
2017-08-01 02:29:56 +0800

    used cfg.xml file to configure hibernate

--- src/main/java/com/xl/data/Application.java
+++ src/main/java/com/xl/data/Application.java
@@ -1,15 +1,23 @@
 package com.xl.data;
 
+import java.util.Date;
+
 import org.hibernate.Session;
 
 import com.xl.data.entities.User;
-
 
 public class Application {
 	
 	public static void main(String[] args){
 		Session session = HibernateUtil.getSessionFactory().openSession();
 		session.beginTransaction();
+//		User user = new User();
+//		user.setBirthDate(new Date());
+//		user.setCreatedBy("me");
+//		user.setCreatedDate(new Date());
+//		user.setLastUpdatedDate(new Date());
+//		session.save(user);
+//		session.getTransaction().commit();
 		session.close();
 	}
 }
--- src/main/java/com/xl/data/HibernateUtil.java
+++ src/main/java/com/xl/data/HibernateUtil.java
@@ -1,30 +1,29 @@
 package com.xl.data;
 
-
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 
-import com.xl.data.entities.User;
-
-public class HibernateUtil {	
+public class HibernateUtil {
 	private static final SessionFactory sessionFactory = buildSessionFactory();
 
 	private static SessionFactory buildSessionFactory() {
 		SessionFactory sessionFactory = null;
 		try {
 			Configuration configuration = new Configuration();
-			configuration.addAnnotatedClass(User.class);
-			sessionFactory = configuration.buildSessionFactory(new StandardServiceRegistryBuilder().build());
-		} catch (Exception ex){
+			sessionFactory = configuration.buildSessionFactory(
+					new StandardServiceRegistryBuilder()
+						.applySettings(configuration.getProperties())
+						.build());
+		} catch (Exception ex) {
 			ex.printStackTrace();
 			throw new RuntimeException("Error building session factory");
 		}
-		
+
 		return sessionFactory;
 	}
-	
-	public static SessionFactory getSessionFactory(){
+
+	public static SessionFactory getSessionFactory() {
 		return sessionFactory;
 	}
 }
